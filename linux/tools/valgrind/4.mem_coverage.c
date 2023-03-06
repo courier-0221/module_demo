cf8bce4c8d99a513849b5b028a0bcb23c1718e4d
courier_0221 <courier_0221@163.com>
2023-03-06 19:49:54 +0800

    调整栈空间数组越界例子

--- linux/tools/valgrind/4.mem_coverage.c
+++ linux/tools/valgrind/4.mem_coverage.c
@@ -4,35 +4,39 @@
 
 int main(void)
 {
-    char xx[50];
-    for (int ii = 0; ii < 50; ++ii) 
-    {
-        xx[ii] = ii;
-    }
-    strncpy(xx + 20, xx, 20);//ok
-    strncpy(xx + 20, xx, 21);//overlap
+    char x[10]; 
+    x[11] = 'a';
 
     return 0;
 }
 
-
 /*
 valgrind --tool=memcheck --leak-check=full ./4_mem_coverage 
-==47558== Memcheck, a memory error detector
-==47558== Copyright (C) 2002-2022, and GNU GPL'd, by Julian Seward et al.
-==47558== Using Valgrind-3.20.0 and LibVEX; rerun with -h for copyright info
-==47558== Command: ./4_mem_coverage
-==47558== 
-==47558== 
-==47558== HEAP SUMMARY:
-==47558==     in use at exit: 0 bytes in 0 blocks
-==47558==   total heap usage: 0 allocs, 0 frees, 0 bytes allocated
-==47558== 
-==47558== All heap blocks were freed -- no leaks are possible
-==47558== 
-==47558== For lists of detected and suppressed errors, rerun with: -s
-==47558== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
-
+==55534== Memcheck, a memory error detector
+==55534== Copyright (C) 2002-2022, and GNU GPL'd, by Julian Seward et al.
+==55534== Using Valgrind-3.20.0 and LibVEX; rerun with -h for copyright info
+==55534== Command: ./4_mem_coverage
+==55534== 
+*** stack smashing detected ***: <unknown> terminated
+==55534== 
+==55534== Process terminating with default action of signal 6 (SIGABRT)
+==55534==    at 0x4E82E87: raise (raise.c:51)
+==55534==    by 0x4E847F0: abort (abort.c:79)
+==55534==    by 0x4ECD836: __libc_message (libc_fatal.c:181)
+==55534==    by 0x4F78B30: __fortify_fail_abort (fortify_fail.c:33)
+==55534==    by 0x4F78AF1: __stack_chk_fail (stack_chk_fail.c:29)
+==55534==    by 0x10869D: main (4.mem_coverage.c:11)
+==55534== 
+==55534== HEAP SUMMARY:
+==55534==     in use at exit: 0 bytes in 0 blocks
+==55534==   total heap usage: 0 allocs, 0 frees, 0 bytes allocated
+==55534== 
+==55534== All heap blocks were freed -- no leaks are possible
+==55534== 
+==55534== For lists of detected and suppressed errors, rerun with: -s
+==55534== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+Makefile:25: recipe for target 'run_4' failed
+make: *** [run_4] Aborted (core dumped)
 
 结果：
     不能发现
